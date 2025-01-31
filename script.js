bbc84e8c0bbd9d373c7b3a2fe95ddb680bcc2207
vatsalatea <96271833+vatsalatea@users.noreply.github.com>
2025-01-31 17:31:43 -0500

    Create script.js
    added js

---
+++ script.js
@@ -0,0 +1,16 @@
+//create const variables for each change button using document.getElementById
+const changeButton1 = document.getElementById('changeButton1')
+
+//create another const variable that retrieves a reference to an HTML element with the id attribute set to "image1" and stores it in a variable
+
+
+// Define alternate image sources
+const image1Alt = "image1_alt.jpg";
+const image2Alt = "image2_alt.jpg";
+const image3Alt = "image3_alt.jpg";
+
+//duplicate this and make it for each of the three changeButtons, images, and imageAlts
+changeButton.addEventListener('click', () => {
+  image.src = imageAlt;
+});
+

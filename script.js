b5d45725fbfada8fae41bbc5051573763d49a4ca
vatsalatea <96271833+vatsalatea@users.noreply.github.com>
2025-02-06 12:04:32 -0500

    Update script.js
    added const var and change button var

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //create const variables for each change button using document.getElementById
 const changeButton1 = document.getElementById('changeButton1')
+const changeButton2 = document.getElementById('changeButton2')
+const changeButton3 = document.getElementById('changeButton3')
 
 //create another const variable that retrieves a reference to an HTML element with the id attribute set to "image1" and stores it in a variable
-
-
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
 // Define alternate image sources
 const image1Alt = "image1_alt.jpg";
 const image2Alt = "image2_alt.jpg";

37a31a67c62ff3ac8502a9c2dc4a790fc6cb7aea
vatsalatea <96271833+vatsalatea@users.noreply.github.com>
2025-02-09 13:47:04 -0500

    Update script.js
    added image alts

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 // Define alternate image sources
-const image1Alt = "image1_alt.jpg";
-const image2Alt = "image2_alt.jpg";
-const image3Alt = "image3_alt.jpg";
+const image1Alt = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Maserati_MC20_IAA_2021_1X7A0087.jpg/1920px-Maserati_MC20_IAA_2021_1X7A0087.jpg";
+const image2Alt = "https://hips.hearstapps.com/hmg-prod/images/brabham-bt62-115-1525366041.jpg";
+const image3Alt = "https://upload.wikimedia.org/wikipedia/commons/1/11/Nissan_Pao_001.JPG";
 
 //duplicate this and make it for each of the three changeButtons, images, and imageAlts
 changeButton.addEventListener('click', () => {
